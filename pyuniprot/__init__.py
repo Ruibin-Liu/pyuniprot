cfc3ad30e80b2c4fa4f0a99c3420ac6914f8d0a1
Ruibin Liu <ruibinliuphd@gmail.com>
2024-04-12 15:47:54 -0400

    Revert "can load UniRef json now"
    This reverts commit aa666a368fe13ec946fb2108731055c065fb8863.

--- pyuniprot/__init__.py
+++ pyuniprot/__init__.py
@@ -1,11 +1,9 @@
 from .Uniprot import Uniprot
-from .UniRef import UniRef
 from .utils import get_alt_resids, get_isoforms
 from .version import __version__
 
 __all__ = [
     "Uniprot",
-    "UniRef",
     "get_isoforms",
     "get_alt_resids",
     "__version__",
