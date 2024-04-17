1e9a3352f951d52ddadb4d76ed60a4482aec4ca0
Ruibin Liu <ruibinliuphd@gmail.com>
2024-04-17 16:10:14 -0400

    Disable legacy Uniprot export

--- pyuniprot/__init__.py
+++ pyuniprot/__init__.py
@@ -1,11 +1,11 @@
-from .Uniprot import Uniprot
+# from .Uniprot import Uniprot
 from .UniProt import UniProt
 from .UniRef import UniRef
 from .utils import get_alt_resids, get_isoforms
 from .version import __version__
 
 __all__ = [
-    "Uniprot",
+    # "Uniprot",
     "UniProt",
     "UniRef",
     "get_isoforms",
